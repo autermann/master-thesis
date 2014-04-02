fe292b0b425ed88209075baa853e25cfb628f587
Christian Autermann <christian@autermann.org>
2014-04-02 16:05:26 +0200

    minor adjustments to example listings

--- thesis/listings/matlab-add-javascript-client.js
+++ thesis/listings/matlab-add-javascript-client.js
@@ -1,22 +1,19 @@
-function createRequest(a, b) {
-  return JSON.stringify({
-    'function': 'add',
-    'parameters': [{ 'type': 'scalar', 'value': a },
-                   { 'type': 'scalar', 'value': b }],
-    'results': { 'result': 'scalar' }
-  });
-}
-
-function parseResponse(e) {
-  return JSON.parse(e.data).results.result.value;
-}
-
 var socket = new WebSocket('ws://localhost:7000');
 
 socket.onmessage = function(e) {
-  console.log(parseResponse(e));
+	var json = JSON.parse(e.data);
+	var result = json.results.result.value;
+	console.log(result);
 };
 
 socket.onopen = function() {
-  this.send(createRequest(1,2));
+	var request = JSON.stringify({
+		'function': 'add',
+		'parameters': [
+			{ 'type': 'scalar', 'value': 1 },
+			{ 'type': 'scalar', 'value': 2 }
+		],
+		'results': { 'result': 'scalar' }
+	});
+	this.send(request);
 };
