14322bb22287bb226c24a66501727d877042975a
Christian Autermann <christian@autermann.org>
2014-04-16 15:16:17 +0200

    missing wps4r example

---
+++ thesis/listings/wps4r-annotations.r
@@ -0,0 +1,10 @@
+# wps.des: id = process, title = "my script",
+#          abstract = "analyze 42 things"
+
+# wps.in: id = myFactor, type = integer, title = "numerical factor",
+#         abstract = "the number to be used for factorization",
+#         value = 1, minOccurs = 0, maxOccurs = 1;
+
+# wps.out: id = myResult, type = string,
+#          title = "factorized output",
+#          abstract = "output number as text in scientific notation (a x 10^b)";
